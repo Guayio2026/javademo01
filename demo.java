347b8713e768e817950ab6d73929753e932adb26
Guayio <1705495004@qq.com>
2026-04-16 21:10:12 +0800

    添加win界面
    只添加了显示界面，还没有做交互

--- demo.java
+++ demo.java
@@ -1,11 +1,30 @@
 package demo;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Scanner;
 public class demo {
   public static void main(String Args[]){
+      JFrame win= new JFrame("家具管理系统");
+      win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      win.setSize(500,500);
+      win.setLocationRelativeTo(null);
+      win.setVisible(true);
+      JPanel panel=new JPanel();
+      win.add(panel);
+      JButton btn=new JButton("登录");
+      panel.add(btn);
+      btn.addActionListener(new ActionListener(){
+
+          @Override
+          public void actionPerformed(ActionEvent e){
+              System.out.println("登录成功");
+          }
+      });//
       JD[]jds=new JD[4];
       jds[0]=new lamp("灯",false,1);
       jds[1]=new tv("电视",false,2);
-      jds[2]=new win("窗",false,3);
+      jds[2]=new wind("窗",false,3);
       jds[3]=new wash("洗刷",false,4);
       smarthomecontrol s=smarthomecontrol.getcontrol();
       while(true)
---
+++ wind.java
@@ -0,0 +1,8 @@
+package demo;
+
+public class wind extends JD{
+    public wind(String name,boolean status,int mark)
+    {
+        super(name,status,mark);
+    }
+}
